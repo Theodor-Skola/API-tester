fa3539df318e8f6eb81180185c367b4a6e054af6
Theodor-Skola <08thewem@skola.boras.se>
2026-04-28 08:44:52 +0200

    dasd

---
+++ apiscript.js
@@ -0,0 +1,34 @@
+
+async function räknaAlgebra() {
+	let input = document.getElementById("algebraInput").value;
+	
+	if (input == "") {
+		console.log("Ingen input");
+	} else {
+		
+		const appId = 'XH2YR8V79R';
+		const encodedQuery = encodeURIComponent(input);
+		const url = 'http://api.wolframalpha.com/v1/result?appid=${appId}&i=${encodedQuery}';
+		// const url = 'http://api.wolframalpha.com/v1/result?appid=XH2YR8V79R&i=How+far+is+Los+Angeles+from+New+York%3f';
+		
+		try {
+			const response = await fetch(url, 
+			{
+				method: "GET", 
+				headers: {
+					"Access-Control-Allow-Origin": "http://api.wolframalpha.com/v1/result"
+				}
+			});
+        
+			if (!response.ok) {
+				throw new Error(`API-fel: ${response.status}`);
+			}
+
+			const answer = await response.text();
+		} catch (error) {
+			console.error('Kunde inte hämta svar:', error);
+		}
+		
+		document.getElementById("algebraSvar").innerText = answer;
+	}
+}
